5b3116fcfa510da0da0518e94c056c5ea8f216eb
Andrius <andrius.bertasius@mif.stud.vu.lt>
2022-03-23 21:23:34 +0200

    Spring boot generated project

---
+++ src/main/java/com/miausoft/miaups/MiaupsApplication.java
@@ -0,0 +1,13 @@
+package com.miausoft.miaups;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaupsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiaupsApplication.class, args);
+	}
+
+}
